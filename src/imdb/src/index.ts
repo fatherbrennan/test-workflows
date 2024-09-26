88e57543ab8fe0ce03dc567d2c6dbcb318979748
fatherbrennan <bjt.lepore@gmail.com>
2024-09-26 14:10:27 +0930

    update source files

--- src/imdb/src/index.ts
+++ src/imdb/src/index.ts
@@ -3,12 +3,12 @@ import { rm } from 'fs/promises';
 import { join, resolve } from 'path';
 
 (async () => {
-  const dir = resolve(__dirname, '..');
+  const dir = resolve(__dirname, '..', '..', '..');
   const dirTmp = join(dir, 'tmp');
 
   await rm(dirTmp, { recursive: true, force: true });
 
   for (let i = 0; i < 10; i++) {
-    await write(join(dirTmp, `${i + 1}.json`), JSON.stringify({ index: i }));
+    await write(join(dirTmp, 'imdb', `${i + 1}.json`), JSON.stringify({ index: i }));
   }
 })();
